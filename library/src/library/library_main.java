a1c86f5893ecf270dabf71d08dfc297e3e4131e0
PHL2000 <118107980+PHL2000@users.noreply.github.com>
2022-12-05 14:54:51 +0900

    Update library_main.java

--- library/src/library/library_main.java
+++ library/src/library/library_main.java
@@ -5,11 +5,11 @@ import java.awt.event.*;
 import java.awt.*;
 
 public class library_main extends JFrame {
-	private ImageIcon adminIcon = new ImageIcon("library/images/dgurogo.png");
-	private ImageIcon backIcon = new ImageIcon("library/images/dgurogo2.png");
-	private JLabel title = new JLabel("<HTML><body><center>동국대학교 중앙도서관<br>키오스크</center></body></HTML>");
-	private MyDialog password;
-	static Database db = new Database();
+	private ImageIcon adminIcon = new ImageIcon("library/images/dgurogo.png"); // 관리자 모드 이미지 아이콘
+	private ImageIcon backIcon = new ImageIcon("library/images/dgurogo2.png"); // 배경 이미지 아이콘
+	private JLabel title = new JLabel("<HTML><body><center>동국대학교 중앙도서관<br>키오스크</center></body></HTML>"); // 제목 
+	private MyDialog password; // 다이얼로그 선언
+	static Database db = new Database(); // db 연동
 
 	// 생성자
 
@@ -20,16 +20,17 @@ public class library_main extends JFrame {
 		c.setBackground(Color.WHITE);
 		c.setLayout(null);
 
-		// 이미지 불러오기
+		// 이미지 불러오기(이미지 크기 재설정): 관리자 버튼 이미지
 		Image adminimg = adminIcon.getImage();
 		adminimg = adminimg.getScaledInstance(48, 48, java.awt.Image.SCALE_SMOOTH);
 		adminIcon = new ImageIcon(adminimg);
 
-		// 이미지 불러오기
+		// 이미지 불러오기(이미지 크기 재설정): 배경 
 		Image backimg = backIcon.getImage();
 		backimg = backimg.getScaledInstance(400, 400, java.awt.Image.SCALE_SMOOTH);
 		backIcon = new ImageIcon(backimg);
-		JLabel back_img = new JLabel(backIcon);
+		// 라벨에 이미지를 넣고, 라벨의 위치를 설정하여 화면 중간에 배치한다.
+		JLabel back_img = new JLabel(backIcon); 
 		back_img.setSize(400, 400);
 		back_img.setLocation(150, 50);
 
@@ -69,19 +70,23 @@ public class library_main extends JFrame {
 		Font btn1_font = new Font("NanumSquare", Font.BOLD, 25);
 		Font btn2_font = new Font("NanumSquare", Font.BOLD, 25);
 
+		// 제목 폰트 및 사이즈, 위치 설정 
 		title.setFont(t_font);
 		title.setLocation(150, 200);
 		title.setSize(410, 100);
-
+		
+		// 버튼 폰트 설정
 		btn1.setFont(btn1_font);
 		btn2.setFont(btn2_font);
 
+		// 창에 컴포넌트 
 		c.add(title);
 		c.add(back_img);
 		c.add(btn1);
 		c.add(btn2);
 		c.add(btn_manager);
 
+		// 다이얼로그 생성 후 관리자 버튼에 리스너 추가
 		password = new MyDialog(this, "관리자 비밀번호");
 		btn_manager.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -94,19 +99,22 @@ public class library_main extends JFrame {
 
 	}
 
+	// 배경 이미지 설정
 	class background extends JPanel {
 		private ImageIcon back_Image = new ImageIcon("library/images/back_img.png");
 		private Image img = back_Image.getImage(); // 이미지 객체
 
 		public void paintComponent(Graphics g) {
 			super.paintComponent(g);
+			// 이미지의 크기를 들어가는 공간의 크기에 맞추고, 들어가는 공간의 x좌표 0, y좌표0 위치시킨다. 
 			g.drawImage(img, 0, 0, getWidth(), getHeight(), this);
 		}
 	}
 
+	// 다이얼로그 창 생성(관리자 모드에 들어가기전 거쳐야 작업)
 	class MyDialog extends JDialog {
 		private JLabel la = new JLabel("비밀번호를 입력하세요.");
-		private JTextField tf = new JTextField(15);
+		private JTextField tf = new JTextField(15); // 비밀 번호 입력 공간
 		private JButton okButton = new JButton("확인");
 
 		public MyDialog(JFrame frame, String title) {
@@ -121,10 +129,11 @@ public class library_main extends JFrame {
 					String input = tf.getText();
 					// setVisible(false);
 					String T_password = "admin";
+					// textfield에 입력된 값의 정해진 비밀번호와 일치하면, 관리자 모드 창을 연다.
 					if (input.equals(T_password)) {
 						new AdminPage();
 						dispose();
-					} else {
+					} else { // 틀리면, 오류 메세지 뜸
 						JOptionPane.showMessageDialog(null, "비밀번호를 다시 입력해주세요.", "Message", JOptionPane.ERROR_MESSAGE);
 					}
 				}
@@ -132,20 +141,22 @@ public class library_main extends JFrame {
 		}
 	}
 
+	// 눌리 버튼의 이름을 인식하여 같은 이름의 창 열기
 	class inActionListener implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
 			JButton b = (JButton) e.getSource();
 			if (b.getText().equals("<HTML><body><center>로그인<br>(대출/연장/예약)</center></body></HTML>")) {
 
 				new login();
-				dispose();
+				dispose();	// 기존 창 닫기
 			} else if (b.getText().equals("반납")) {
 				new returnBook();
-				setVisible(false);
+				setVisible(false);	// 기존 창 닫기: dispose();와 같은 역할을 함
 			}
 		}
 	}
 
+	// 이곳에서 윈도우창 실행
 	public static void main(String[] args) {
 		db.logout();
 		new library_main();
